ca96b7af5e0eb016252e35d74f98fc95ba26f398
fread007 <siansigsavourat@gmail.com>
2024-02-13 15:54:38 +0100

    ajout de la redirection d'entree et sortie

--- fichiers_fournis/src/shell.c
+++ fichiers_fournis/src/shell.c
@@ -11,11 +11,32 @@ void clear_pid(){
 	while(waitpid(-1,NULL,0)!=-1){}
 }
 
+void clear_redirection(struct cmdline *l , int doc_in, int doc_out){
+	if(l->in != NULL){
+		close(doc_in);
+		l->in = NULL;
+		dup2(5,0);
+	}
+	if(l->out != NULL){
+		close(doc_out);
+		l->out = NULL;
+		dup2(6,1);
+	}
+}
+
+void exec_quit(){
+	printf("quit\n");
+	clear_pid();
+	exit(0);
+}
+
+
 int main()
 {
 	while (1) {
 		struct cmdline *l;
 		int i, j;
+		int doc_in,doc_out;
 
 		printf("kintama> ");
 		l = readcmd();
@@ -34,12 +55,15 @@ int main()
 
 		if (l->in != NULL) {
 			printf("in: %s\n", l->in);
-			int doc_in = open(l->in,O_RDONLY);
+			doc_in = open(l->in,O_RDONLY,0);
+			dup2(0,5);
 			dup2(doc_in,0);
+
 		}
 		if (l->out != NULL) {
 			printf("out: %s\n", l->out);
-			int doc_out = open(l->out,O_WRONLY ,O_CREAT);
+			doc_out = open(l->out,O_WRONLY | O_CREAT | O_TRUNC,0752);
+			dup2(1,6);
 			dup2(doc_out,1);
 		}
 
@@ -47,22 +71,24 @@ int main()
 		/* Display each command of the pipe */
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-			if(!strcmp(l->seq[i][0],"quit")){
-				printf("quit\n");
-				clear_pid();
-				exit(0);
-			}
-			
-			for (j=0; cmd[j]!=0; j++) {
-				printf("%s ", cmd[j]);
-			}
 
 			if(Fork()==0){
 				execvp(cmd[0],cmd);
 				exit(0);
 			}
+
 			clear_pid();
+			clear_redirection(l,doc_in,doc_out);
+
+			printf("seq[%d]: ", i);
+
+			if(!strcmp(l->seq[i][0],"quit")){
+				exec_quit();
+			}
+			
+			for (j=0; cmd[j]!=0; j++) {
+				printf("%s ", cmd[j]);
+			}
 
 			printf("\n");
 		}
