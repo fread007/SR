bb05a86ddf6054dc0a3fdb7537dbeab853418c9d
fread007 <siansigsavourat@gmail.com>
2024-02-20 11:59:51 +0100

    fusion entre la gestion d'erreur et les pipe

--- fichiers_fournis/src/shell.c
+++ fichiers_fournis/src/shell.c
@@ -9,18 +9,48 @@
 
 #define FIFO_NAME "./fifo/fifo%d"
 
+void debbug(int val_retour, char* nom){
+	switch (val_retour) {
+		case 1:
+			printf("%s: command not found.\n", nom);
+			break;
+		case 2:
+			switch (errno) {
+				case EACCES:
+					printf("%s: Permission denied.\n", nom);
+					break;
+				case ENOENT:
+					printf("%s: File not found.\n", nom);
+					break;
+				default : 
+					printf("%s: Error number %d.\n", nom, errno);
+					break;
+			}
+			break;
+		case 3:
+			printf("%s: Closing file error number %d.\n", nom, errno);
+	}	
+}
+
 void clear_pid(){
 	while(waitpid(-1,NULL,0)!=-1){}
 }
 
+
 void clear_redirection(struct cmdline *l , int doc_in, int doc_out){
 	if(l->in != NULL){
-		close(doc_in);
+		int e = close(doc_in);
+		if (e == -1) {
+			debbug(3, l->in);
+		}		
 		l->in = NULL;
 		dup2(5,0);
 	}
 	if(l->out != NULL){
-		close(doc_out);
+		int e = close(doc_out);
+		if (e == -1) {
+			debbug(3, l->in);
+		}
 		l->out = NULL;
 		dup2(6,1);
 	}
@@ -61,6 +91,9 @@ int main()
 		if (l->in != NULL) {
 			printf("in: %s\n", l->in);
 			doc_in = open(l->in,O_RDONLY,0);
+			if (doc_in == -1) {
+				debbug(2, l->in);
+			}
 			dup2(0,5);
 			dup2(doc_in,0);
 
@@ -107,7 +140,10 @@ int main()
 					fifo_fd=open(fifo_actuel_name,O_WRONLY); 
 					dup2(fifo_fd,1);
 				}
-				execvp(cmd[0],cmd);
+				int e=execvp(cmd[0],cmd);
+				if (e == -1) {
+					debbug(1, cmd[0]);
+				}
 				exit(0);
 				
 			}
